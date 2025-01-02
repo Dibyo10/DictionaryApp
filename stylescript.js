3a600235027fa4c8c0bb92e1ccaa8523ba012b69
Dibyo10 <dibyo.dc@gmail.com>
2025-01-03 01:48:58 +0530

    Update stylescript.js

--- stylescript.js
+++ stylescript.js
@@ -1,31 +1,38 @@
-//searchinput
-//searchbtn
-
-const dictionary=(word)=>{
-    
+const dictionary = (word) => {
     const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': '6836099e3dmsh71e2c492b12da9dp10cb2fjsn09008bf7a49b',
-            'x-rapidapi-host': 'dictionary-by-api-ninjas.p.rapidapi.com'
-        }
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '6836099e3dmsh71e2c492b12da9dp10cb2fjsn09008bf7a49b',
+        'x-rapidapi-host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+      }
     };
-    
-    fetch('https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=' + word,options)
-      .then(response=>response.json())
-      .then((response)=>{
-    
-        console.log(response)
-        wordheading.innerHTML="Meaning of : "+response.word
-        definition.innerHTML=response.definition.replace("2.","<br>2.").replace("3.","<br>3.").replace("4.","<br>4.")
+  
+    fetch('https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=' + word, options)
+      .then(response => response.json())
+      .then((response) => {
+        console.log(response);
+        wordheading.innerHTML = "Meaning of : " + response.word;
+        definition.innerHTML = response.definition.replace("2.", "<br>2.").replace("3.", "<br>3.").replace("4.", "<br>4.");
       })
-    
-      .catch(err=>console.error(err));
-    
-    }
-    
-    searchbtn.addEventListener('click',(e)=>{
-        e.preventDefault();
-        dictionary(searchinput.value)
-    
-    })
+      .catch(err => console.error(err));
+  };
+  
+  searchbtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    dictionary(searchinput.value);
+  });
+  
+  
+  const aboutbtn = document.getElementById('aboutbtn');
+  const aboutModal = new bootstrap.Modal(document.getElementById('aboutModal'));
+  
+  aboutbtn.addEventListener('click', () => {
+    aboutModal.show();
+  });
+  
+  
+  const homebtn = document.getElementById('homebtn');
+  homebtn.addEventListener('click', () => {
+    window.location.href = 'index.html'; 
+  });
+  
